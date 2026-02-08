7451bed859147368d96be64360bd0ad288225f52
kubanabaraka <byukusengekubanabarakapascal@gmail.com>
2026-02-08 22:16:40 +0200

    Setup Environment

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello, TypeScript!");
+let age = 21;
+if (age < 50) {
+    age += 10;
+    console.log(`You are ${age} years old.`);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello, TypeScript!");
+let age: number = 21;
+if (age < 50) {
+  age += 10;
+  console.log(`You are ${age} years old.`);
+}
