adf1adf247a3d43cdc262a7883b160ee18a96da8
kubanabaraka <byukusengekubanabarakapascal@gmail.com>
2026-02-11 20:04:02 +0200

    Add a function

---
+++ func.js
@@ -0,0 +1,5 @@
+var greet;
+greet = function (name) {
+    return "Hello ".concat(name, " you are welcome to TypeScript");
+};
+console.log(greet("Baraka"));
---
+++ func.ts
@@ -0,0 +1,6 @@
+let greet : (name: string)=> string;
+
+greet = (name: string)=>{
+    return `Hello ${name} you are welcome to TypeScript`;
+}
+console.log(greet("Baraka"));
